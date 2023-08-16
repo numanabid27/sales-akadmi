1b247eb4fe950f7e5c6c95f70e4b9c2b7f1d8e26
numanabid27 <numanabid27@gmail.com>
2023-08-16 06:47:41 -0700

    home page done

---
+++ main.js
@@ -0,0 +1,20 @@
+$('.owl-carousel.blogs').owlCarousel({
+    loop:false,
+    margin:30,
+    dots:false,
+    nav:false,
+    stagePadding: 70,
+    responsive:{
+        0:{
+            items:1,
+           
+        },
+        
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})   
